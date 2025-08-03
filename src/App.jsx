9dc16fcb944daace34017d687973908b19fa7870
vinit <vinitkumar93341@gmail.com>
2025-08-03 14:35:34 +0530

    navbar full responsive

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <main className='text-sm text-neutral-300 antialiased'>
+        <Navbar/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import { RiCloseFill, RiMenu3Fill } from "@remixicon/react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  return (
+    <nav className="fixed top-4 left-0 right-0 z-50 m-2">
+      <div
+        className="text-neutral-500 bg-black/60 backdrop-blur-md
+      max-w-7xl mx-auto px-4 py-3 flex justify-between items-center
+      rounded-xl border border-neutral-800"
+      >
+        {/* left logo */}
+        <img src={logo} alt="" width="120" height="24" />
+
+        {/* center links hidden on mobile view */}
+        <div className="hidden md:flex space-x-7">
+          <a href="#works" className="hover:text-neutral-200">
+            How it works
+          </a>
+          <a href="#pricing" className="hover:text-neutral-200">
+            Pricing
+          </a>
+          <a href="testimonilas" className="hover:text-neutral-200">
+            Testimonials
+          </a>
+        </div>
+
+        {/* right buttons hidden on mobile */}
+        <div className="hidden md:flex space-x-4 items-center">
+          <a href="" className="hover:text-neutral-200">
+            Login
+          </a>
+          <a
+            href=""
+            className="border border-neutral-700 text-white py-2 px-4
+                rounded-lg hover:bg-neutral-700 transition"
+          >
+            Get a Demo
+          </a>
+          <a
+            href=""
+            className="bg-blue-600 text-white py-2 px-4 rounded-lg
+                hover:bg-blue-500 transition"
+          >
+            Start a free trial
+          </a>
+        </div>
+
+        {/* hamburger only visible for mobile */}
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white
+                focus:outline-none"
+          >
+            {isOpen ? <RiCloseFill /> : <RiMenu3Fill />}
+          </button>
+        </div>
+      </div>
+
+      {/* mobile menu */}
+      {isOpen && (
+        <div
+          className=" md:hidden bg-neutral-900/60 backdrop-blur-md border
+          border-neutral-800 p-4 rounded-xl mt-2"
+        >
+          <div className="flex flex-col space-y-4">
+            <a href="#" className="hover:text-neutral-200">
+              Product
+            </a>
+            <a href="#" className="hover:text-neutral-200">
+              Pricing
+            </a>
+            <a href="#" className="hover:text-neutral-200">
+              Resources
+            </a>
+            <a href="#" className="hover:text-neutral-200">
+              Login
+            </a>
+            <a
+              href=""
+              className="border border-neutral-700 text-white py-2 px-4
+                rounded-lg hover:bg-neutral-700 transition"
+            >
+              Get a Demo
+            </a>
+            <a
+              href=""
+              className="bg-blue-600 text-white py-2 px-4 rounded-lg
+                hover:bg-blue-500 transition"
+            >
+              Start a free trial
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
